f36c9c0294d5b125eb41fc0f3d7e11297de13d56
Julius Pedersen <deifyed@tutanota.com>
2022-09-24 18:51:28 +0200

    ✅ Add tbltest golang template

---
+++ plugin/julius/templates.vim
@@ -0,0 +1,1 @@
+inoremap tbltest <ESC>:r ~/.config/nvim/plugin/julius/templates/golang/tbltest.go<CR>kddji<TAB><TAB>
---
+++ plugin/julius/templates/golang/tbltest.go
@@ -0,0 +1,14 @@
+	testCases := []struct {
+
+	}{
+		{
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+                        t.Parallell()
+		})
+	}
