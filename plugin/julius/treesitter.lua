a3853ce069ae8d0d8d95728f180e3a8a13eb7c65
Julius Pedersen <deifyed@tutanota.com>
2022-12-14 19:51:39 +0100

    ✅ Add vue lang to treesitter ensure installed

--- plugin/julius/treesitter.lua
+++ plugin/julius/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "go", "javascript" },
+  ensure_installed = { "go", "javascript", "vue" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
