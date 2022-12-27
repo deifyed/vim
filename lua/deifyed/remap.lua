70268618376d945ae44e8fc55db8ddb73a6f29de
Julius Pedersen <deifyed@tutanota.com>
2022-12-27 15:31:11 +0100

    ✅ Add shortcut for closing buffers

--- lua/deifyed/remap.lua
+++ lua/deifyed/remap.lua
@@ -1,7 +1,16 @@
 vim.g.mapleader = " "
+--
+-- Open treeview
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Move selected lines up or down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Disable something
 vim.keymap.set("n", "Q", "<nop>")
+
+-- Buffer navigation
+vim.keymap.set("n", "<leader>q", function()
+    vim.api.nvim_buf_delete(0, {})
+end)
